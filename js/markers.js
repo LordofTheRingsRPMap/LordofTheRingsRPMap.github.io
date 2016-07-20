05493becf5363f2bae9a95c24eac6942e3563feb
Astos <hdpenney@gmail.com>
2016-07-19 21:56:38 -0500

    Map and interface updated to Lotr and basic marker implementation.

---
+++ js/markers.js
@@ -0,0 +1,59 @@
+  /********************************************/
+ /**				MARKERS					**/
+/********************************************/
+
+// Define marker icons
+// For each region add an image to 'img/marker/' 
+var Gondor = L.icon({iconUrl: 'img/marker/Gondor.png',iconSize: [21, 21]})
+
+var Rohan = L.icon({iconUrl: 'img/marker/Rohan.png',iconSize: [21, 21]})
+
+var DarkForces = L.icon({iconUrl: 'img/marker/DarkForces.png',iconSize: [21, 21]})
+
+var BlueDwarves = L.icon({iconUrl: 'img/marker/Bl.png',iconSize: [21, 21]})
+var MoriaDwarves = L.icon({iconUrl: 'img/marker/MoriaDwarves.png',iconSize: [21, 21]})
+
+var DarkMen = L.icon({iconUrl: 'img/marker/DarkMen.png',iconSize: [21, 21]})
+var Umbar = L.icon({iconUrl: 'img/marker/Umbar.png',iconSize: [21, 21]})
+
+var Arnor = L.icon({iconUrl: 'img/marker/Arnor.png',iconSize: [21, 21]})
+var Shire = L.icon({iconUrl: 'img/marker/Shire.png',iconSize: [21, 21]})
+
+var Lothlorien = L.icon({iconUrl: 'img/marker/Elves.png',iconSize: [21, 21]})
+var Rivendell= L.icon({iconUrl: 'img/marker/HighElves.png',iconSize: [21, 21]})
+
+var Dale = L.icon({iconUrl: 'img/marker/Dale.png',iconSize: [21, 21]})
+
+var Lindon = L.icon({iconUrl: 'img/marker/Elves.png',iconSize: [21, 21]})
+
+var WoodElves = L.icon({iconUrl: 'img/marker/WoodElves.png',iconSize: [21, 21]})
+var Radagast = L.icon({iconUrl: 'img/marker/Istari.png',iconSize: [21, 21]})
+
+// Define the markers
+var markers_Gondor = [
+
+[-31.428663117358596,-114.697265625,Gondor,"<b>Lover's Hill</b><br>- House: Lonmouth<br>- Levies: 500"]]
+
+
+var markers_Vale = []
+
+var markers_North = []
+
+var markers_CL = []
+
+//Reach
+
+var markers_Reach = []
+//Iron Islands
+var markers_II = []
+
+var markers_Dorne = []
+
+//Riverlands
+var markers_RL = []
+
+var markers_West = []
+
+var markers_NonWesteros = []
+
+var markers_Logos = []
